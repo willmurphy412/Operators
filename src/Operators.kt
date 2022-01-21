52505ecdf430ecacff95b29fc74d8bd7559b4b14
willmurphy412 <wmurphy@students.mchenry.edu>
2022-01-20 22:31:43 -0600

    Initial commit

---
+++ src/Operators.kt
@@ -0,0 +1,32 @@
+fun main(){
+    //prints 2 + 3 = 5
+    var a = 2
+    var b = 3
+    var answer = a + b
+    println("" + a + " + " + b + " = " + answer)
+
+    //prints 6 * 1 = 6
+    a *= 3
+    b /= 3
+    answer = a * b
+    println("" + a + " * " + b + " = " + answer)
+
+    //prints 10 - 4 = 6
+    a += 4
+    b += 3
+    answer = a - b
+    println("" + a + " - " + b + " = " + answer)
+
+    //prints 25 / 5 = 5
+    a += 15
+    b += 1
+    answer = a / b
+    println("" + a + " / " + b + " = " + answer)
+
+    //prints 5 % 2 = 1
+    a /= 5
+    b -= 3
+    answer = a % b
+    println("" + a + " % " + b + " = " + answer)
+
+}
